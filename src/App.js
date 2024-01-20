7ef41849d6432c62833ee15660ce139bd99929fe
Ashkan Forghani <ashkanf@hotmail.com>
2024-01-20 12:59:57 -0500

    test1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Test</Text>
+
       <StatusBar style="auto" />
     </View>
   );
