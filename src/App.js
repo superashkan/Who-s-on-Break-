0aaa45d45f19bc0d2fb0e47b6ce48d0b3a374e34
Ahmad Nabelsi <ahmad@wearecws.com>
2024-01-20 12:55:31 -0500

    src files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Text>GG boys
+
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
